c1cbc71332625e690cda1fc7ac8a3fa55acdddcf
Nicolas Vasquez <n.vasquez10@uniandes.edu.co>
2017-03-27 12:20:22 -0500

    Ultimo cambio thumbnail

--- imports/components/futbolYa.js
+++ imports/components/futbolYa.js
@@ -8,7 +8,7 @@
         <div className="container">
         <h1 className="tit">Fútbol Ya!</h1>
         <h3 className="subTit">
-          <span><strong>Fútbol Ya</strong></span> es una página que te permite encontrarte con todos los espacios que quieras para jugar fútbol, puedes encontrar equipos que estén buscando gente, o puedes crear tu propia sesión reservando una cancha e invitando a los demás a que se animen a jugar contigo. ¡Inténtalo!
+          <strong><span>Fútbol Ya</span></strong> es una página que te permite encontrarte con todos los espacios que quieras para jugar fútbol, puedes encontrar equipos que estén buscando gente, o puedes crear tu propia sesión reservando una cancha e invitando a los demás a que se animen a jugar contigo. ¡Inténtalo!
         </h3>
         <p className="derecha amarillo">
           ¿Ya tienes equipo? <a onClick={this.props.irACuadro} href="#" title="Haz click aqui para ir a tu reserva" className="ve">
