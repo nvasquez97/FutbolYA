50e582f3906031ab772b46de6bc3af75d49c0f52
Nicolas Vasquez <n.vasquez10@uniandes.edu.co>
2017-03-27 13:27:34 -0500

    Server imports partidos

--- server/main.js
+++ server/main.js
@@ -3,6 +3,7 @@ import {LocalidadM} from '../imports/api/localidades.js'
 import {Reservas} from '../imports/api/reservas.js'
 import {Canchas} from '../imports/api/canchas.js'
 import {Usuarios} from '../imports/api/usuarios.js'
+import {Partidos} from '../imports/api/partidos.js'
 
 
 Meteor.startup(() => {
