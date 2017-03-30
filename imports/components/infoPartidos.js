ad6d95fc83b306fd46905043f01218bd488fd4e4
Laura <l.castro12@uniandes.edu.co>
2017-03-30 10:52:08 -0500

    Fix futbol

--- imports/components/infoPartidos.js
+++ imports/components/infoPartidos.js
@@ -5,30 +5,26 @@ export default class InfoPartidos extends Component {
     render() {
         return (
             <div className="infoPartidos" >
-            <div className='row'>
-            <div className='col-lg-10'>
             <h1 className="titulo">Lleva el puntaje de tu partido!</h1>
-            </div>
-            </div>
             <div className="puntaje">
             <h className="reservaPartido">Reserva: 1</h><br></br>
             <br></br>
             <div className='row'>
-            <div className='col-md-5'>
+            <div className='col-md-4'>
             <div className="izquierda">
             <label className="equipo">Equipo #1</label>
-            <label className="goles">3</label>
-            <i id="gol1" className="fa fa-futbol-o fa-3x"></i>
+            <label className="goles" >3</label>
+            <i className="fa fa-futbol-o fa-3x"></i>
             </div>
             </div>
-            <div className='col-md-1'>
+            <div className='col-md-4'>
             <div className="centro">
-            <p>
+            <h3>
             VS.
-            </p>
+            </h3>
             </div>
             </div>
-            <div className='col-md-5'>
+            <div className='col-md-4'>
             <div className="derechaD">
             <label className="equipo">Equipo #2</label>
             <label className="goles">2</label>
