af71216a62af2d74a7377071aff1799d9e0c988c
Laura <l.castro12@uniandes.edu.co>
2017-04-20 10:22:07 -0500

    Tests

--- imports/api/usuarios.tests.js
+++ imports/api/usuarios.tests.js
@@ -3,17 +3,17 @@ import { Meteor } from 'meteor/meteor';
 import { chai } from 'meteor/practicalmeteor:chai';
 import { Usuarios } from './usuarios.js';
 
-
-describe('Usuario', function () {
-  beforeEach(function () {
-    Usuarios.remove({});
-    Usuarios.insert({ _id: '1', nombre: 'NVasq' });
-    Usuarios.insert({ _id: '2', nombre: 'LCast' });
+if (Meteor.isServer) {
+  describe('Usuario', function () {
+    beforeEach(function () {
+      Usuarios.remove({});
+      Usuarios.insert({ _id: '1', nombre: 'NVasq' });
+      Usuarios.insert({ _id: '2', nombre: 'LCast' });
+    });
+    it('Should find usuarios', function () {
+      const result = Usuarios.find({});
+      chai.assert.equal(result.count(), 2);
+    });
   });
+}
 
-
-  it('Should find usuarios', function () {
-    const result = Usuarios.find({});
-    chai.assert.equal(result.count(), 2);
-  });
-});
