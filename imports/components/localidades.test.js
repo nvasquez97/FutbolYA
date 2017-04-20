d787a68a7498c4eeafa530d204b305b348b6f740
Nicolas Vasquez <n.vasquez10@uniandes.edu.co>
2017-04-20 18:37:04 -0500

    Test localidad componente renderiza adecuadamente`

--- imports/components/localidades.test.js
+++ imports/components/localidades.test.js
@@ -19,6 +19,8 @@ if(Meteor.isClient){
   			const localidad = Factory.build('local',{"_id": 1, "key": 1, "ubicacion": "San Cristobal"});
   			const result = shallow(<Localidad localidad={localidad} /> );
   			chai.assert.equal(result.node.props.children, 'San Cristobal');
+  			chai.assert.equal(result.instance().props.localidad.ubicacion, 'San Cristobal');
+  			chai.assert.equal(result.instance().props.localidad.key, 1);
   		});
 	});
 }
