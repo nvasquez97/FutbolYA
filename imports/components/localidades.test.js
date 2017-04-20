999a62f978942593cf1cd515dfe834442d26fd92
Nicolas Vasquez <n.vasquez10@uniandes.edu.co>
2017-04-20 14:49:02 -0500

    Tests Render iniciados

--- imports/components/localidades.test.js
+++ imports/components/localidades.test.js
@@ -1,12 +1,27 @@
 /* eslint-env mocha */
 import { Factory } from 'meteor/dburles:factory';
 import { chai } from 'meteor/practicalmeteor:chai';
+import React from 'react';
 import Localidad from './localidad.js'
 import { LocalidadM } from '../api/localidades.js'
-
+import { shallow } from 'enzyme';
 
 describe('Localidades', () => {
-  it('should render', () => {
+  it('Obtiene localidades', () => {
     chai.assert(true);
   });
 });
+
+
+if(Meteor.isClient)
+{
+	Factory.define("local", LocalidadM, {});
+	describe('Localidades', () => {
+  		it('Renderiza localidades', () => {
+  			const localidad = Factory.build('local',{"_id": 1, "key": 1, "ubicacion": "San Cristobal"});
+  			const result = shallow(<Localidad localidad={localidad} /> );
+    	chai.assert(true);
+  		});
+	});
+
+}
